ce6db6c464059475cb6022cfa20d093b211cd1f8
Antisr <55559218+Antisr@users.noreply.github.com>
2020-02-03 15:19:22 +0100

    Add files via upload

--- spegelboll.m
+++ spegelboll.m
@@ -22,7 +22,7 @@ close all
 
 P = I_P - I_S;    # Light-source coords
 
-s_points = fib_sphere(N, r)´
+s_points = fib_sphere(N, r);
 
 # Riktningsvektor för linje från ljuskälla till l
 # V
@@ -50,7 +50,7 @@ for i = 1:size(s_points,1)
     a = [P(1), p_i(1)] + adder(1, :);
     b = [P(2), p_i(2)] + adder(2, :);
     c = [P(3), p_i(3)] + adder(3, :);
-    plot3(a, b, c, 'r')
+    plot3(a, b, c, 'y')
     
     # Spegling kring en normal ges av R = V - 2proj_n(V)
     # Normalvektor --> p_i eftersom det är en sfär med centrum i origo.
@@ -60,9 +60,14 @@ for i = 1:size(s_points,1)
     b2 = [p_i(2), L * ref(2) + p_i(2)] + adder(2, :);
     c2 = [p_i(3), L * ref(3) + p_i(3)] + adder(3, :);
     
-    c = (-1) * dot(lv(i,:),p_i)/(norm(lv(i,:)) * norm(p_i));
+    beta = (-1) * dot(lv(i,:),p_i)/(norm(lv(i,:)) * norm(p_i));
+    color = [1 - beta, 0.3, beta];
     
-    plot3(a2, b2, c2, 'color', [1 - c, 0.5, c], 'LineWidth', 2)
+    # spectrum = jet(100);
+    # color = spectrum(round(100 * beta + 0.5), :);
+    
+    plot3(a, b, c, 'color', color)
+    plot3(a2, b2, c2, 'color', color, 'LineWidth', 2)
   endif
 endfor
 
@@ -72,10 +77,13 @@ a = [I_S(1) - d, I_S(1) + d, I_S(2) - d, I_S(2) + d, I_S(3) - d, I_S(3) + d];
 
 axis(a)
 
-plot3(s_points(:, 1) + I_S(1), s_points(:, 2) + I_S(2), s_points(:, 3) + I_S(3), 'g*')
+# plot3(s_points(:, 1) + I_S(1), s_points(:, 2) + I_S(2), s_points(:, 3) + I_S(3), 'g*')
 
 [x, y, z] = sphere;
 
-surf(r*x + I_S(1), r*y + I_S(2), r*z + I_S(3), 'facecolor', [0,0,0]);
+surf(r*x + I_S(1), r*y + I_S(2), r*z + I_S(3), 'edgecolor', 'none', 'facecolor', [0.8,0.8,0.8]);
 
 hold off
+
+
+# spegelboll([-12, 2, 51], [12, 20, 30], 10, 200)
